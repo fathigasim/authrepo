2a06cca050374fa5e24adb23ff1cb995fb302526
[Mohammed Fathi] <[test@email.com]>
2025-09-05 19:32:44 +0200

    Addin Rainy

--- FullAuth/Controllers/WeatherForecastController.cs
+++ FullAuth/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FullAuth.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Rainy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
